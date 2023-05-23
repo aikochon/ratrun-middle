9f42193cfa1154b3dd0f226d1976823c564293ce
aikochon <trashbinwm@gmail.com>
2023-05-23 14:30:08 +0900

    その他設定反映

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+
+// })
+
+export default defineNuxtConfig({
+    css: ["assets/scss/main.scss"],
+    
+    ssr: false,
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/assets/scss/common.scss";',
+                },
+            },
+        },
+    },
+});
