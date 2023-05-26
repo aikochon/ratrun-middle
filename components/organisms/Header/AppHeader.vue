b4e44dd09182aab1de68c6ab6cde465f56476ea1
aikochon <trashbinwm@gmail.com>
2023-05-26 12:56:41 +0900

    ヘッダー完成

--- components/organisms/Header/AppHeader.vue
+++ components/organisms/Header/AppHeader.vue
@@ -1,18 +1,39 @@
 <template>
-    <div>
-
-        <p>こちらがヘッダーコンポーネントです</p>
-
-    </div>
+  <div>
+    <nav>
+      <li v-for="(link, key) in LinkList" :key="key">
+        <nuxt-link :to="link.to">{{ link.text }}</nuxt-link>
+      </li>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-
+  setup() {
+    const LinkList = reactive([
+      { text: "ホーム", to: "#" },
+      { text: "経営理念", to: "#" },
+      { text: "事業理念", to: "#" },
+      { text: "事業目的", to: "#" },
+      { text: "お問合せ", to: "#" },
+    ]);
+    return { LinkList };
+  },
 };
-
 </script>
 
 <style lang='scss' scoped>
-
+nav {
+  display: flex;
+  gap: 20px;
+  padding: 30px 20px;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
 </style>
