37c97f942f715b4ec45cb0355d0e39d44a0cf9dc
aikochon <trashbinwm@gmail.com>
2023-06-08 21:48:13 +0900

    CSS 修正

--- components/organisms/Header/AppHeader.vue
+++ components/organisms/Header/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header-box">
+  <div>
     <div>
       <nav>
         <li v-for="(link, key) in LinkList" :key="key">
@@ -7,22 +7,10 @@
         </li>
       </nav>
     </div>
-    <div class="aboutus-area">
-      <div class="aboutus-title">
-        <p>理念の文章が入ります</p>
-      </div>
-      <div class="aboutus-detail">
-        <p>
-          母体が内装解体専門業者であるからこそ、一般的な施工会社よりも解体費用を抑えることができます。
-          つまり、解体費用を抑えることで、余力のある資金が生まれ、使い道を広げることができるということです。<br />
-          選択のできる予算の使途も合わせてご提案いたします。
-        </p>
-      </div>
-    </div>
   </div>
 </template>
-    
-    <script>
+      
+      <script>
 export default {
   setup() {
     const LinkList = reactive([
@@ -36,24 +24,16 @@ export default {
   },
 };
 </script>
-    
-    <style lang='scss' scoped>
-.header-box {
-  background-color: darkslateblue;
-  color: white;
-}
-.aboutus-area {
-  width: 400px;
-  padding: 50px 100px 200px 0px;
-  margin: 0 0 0 auto;
-}
-.aboutus-title {
-  font-size: 30px;
-}
+      
+  <style lang='scss' scoped>
 nav {
   display: flex;
   gap: 20px;
   padding: 30px 20px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 1;
 }
 li {
   list-style: none;
